31c81b8a1cd7d10c471165c77cd062a5a2789355
PrzeChoj <premysl.choj@gmail.com>
2023-03-02 11:37:29 +0100

    Poprawiona pierwsza czesc lab1

--- Lab02/ConsoleApp2/ConsoleApp2/Lab02.cs
+++ Lab02/ConsoleApp2/ConsoleApp2/Lab02.cs
@@ -16,9 +16,14 @@ namespace Lab02
         public (bool result, string path) Lab02Stage1(int n, int m, (int, int)[] obstacles)
         {
             bool[,] dynamicPossible = new bool[n, m];
-            string[,] dynamicPath  = new string[n, m];
+            char[,] dynamicPath  = new char[n, m];
+            
+            /// Trasa zapisywana jako liczba int.
+            /// Jesli w dol,    to 'D'
+            /// Jesli w prawo,  to 'R'
+            /// Jak sie nie da, to 'N'
 
-            dynamicPath[0, 0] = "";
+            dynamicPath[0, 0] = 'N';
             
             /// Na poczatek wpisze w dynamicPossible False jesli jest przeszkoda i False jesli nie ma
             for (int i = 0; i < n; i++)
@@ -40,12 +45,12 @@ namespace Lab02
                 if (!dynamicPossible[0, j])
                 {
                     // dynamicPossible[0, j] juz ma false
-                    dynamicPath[0, j] = "";
+                    dynamicPath[0, j] = 'N';
                     continue;
                 }
 
                 dynamicPossible[0, j] = dynamicPossible[0, j - 1];
-                dynamicPath[0, j] = dynamicPath[0, j - 1] + "R";
+                dynamicPath[0, j] = 'R';
             }
             
             /// Pierwszy wiersz jest ok
@@ -55,12 +60,12 @@ namespace Lab02
                 if (!dynamicPossible[i, 0])
                 {
                     // dynamicPossible[i, 0] juz ma false
-                    dynamicPath[i, 0] = "";
+                    dynamicPath[i, 0] = 'N';
                     continue;
                 }
 
                 dynamicPossible[i, 0] = dynamicPossible[i - 1, 0];
-                dynamicPath[i, 0] = dynamicPath[i - 1, 0] + "D";
+                dynamicPath[i, 0] = 'D';
             }
             
             /// Pierwszy wiersz i pierwsza kolumna juz sa ok
@@ -73,31 +78,54 @@ namespace Lab02
                     if (!dynamicPossible[i, j])
                     {
                         // dynamicPossible[i, j] juz ma false
-                        dynamicPath[i, j] = "";
+                        dynamicPath[i, j] = 'N';
                         continue;
                     }
 
                     if (dynamicPossible[i - 1, j])
                     {
                         // dynamicPossible[i, j] juz ma true
-                        dynamicPath[i, j] = dynamicPath[i - 1, j] + "D";
+                        dynamicPath[i, j] = 'D';
                         continue;
                     }
                     
                     if (dynamicPossible[i, j - 1])
                     {
                         // dynamicPossible[i, j] juz ma true
-                        dynamicPath[i, j] = dynamicPath[i, j - 1] + "R";
+                        dynamicPath[i, j] = 'R';
                         continue;
                     }
                     
                     // nie da sie dojsc
                     dynamicPossible[i, j] = false;
-                    dynamicPath[i, j] = "";
+                    dynamicPath[i, j] = 'N';
                 }
             }
+
+            if (!dynamicPossible[n - 1, m - 1])
+            {
+                return (false, "");
+            }
             
-            return (dynamicPossible[n-1, m-1], dynamicPath[n-1, m-1]);
+            /// Zostalo spisac cala trase w string
+            StringBuilder trasaString = new StringBuilder();
+            int ii = n - 1;
+            int jj = m - 1;
+            while (ii != 0 || jj != 0)
+            {
+                if (dynamicPath[ii, jj] == 'R')
+                {
+                    trasaString.Append("R");
+                    jj--;
+                }
+                else
+                {
+                    trasaString.Append("D");
+                    ii--;
+                }
+            }
+
+            return (dynamicPossible[n-1, m-1], new string(trasaString.ToString().Reverse().ToArray()));
         }
 
         /// <summary>
