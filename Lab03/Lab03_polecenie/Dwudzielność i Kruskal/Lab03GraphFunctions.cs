f6e9b67411c23b92642089e3a681126599e1ce60
PrzeChoj <premysl.choj@gmail.com>
2023-03-13 07:53:31 +0100

    Dodanie Lab03 polecenia

---
+++ Lab03/Lab03_polecenie/Dwudzielność i Kruskal/Lab03GraphFunctions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using ASD.Graphs;
+using ASD;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+    public class Lab03GraphFunctions : System.MarshalByRefObject
+    {
+
+        // Część 1
+        // Wyznaczanie odwrotności grafu
+        //   0.5 pkt
+        // Odwrotność grafu to graf skierowany o wszystkich krawędziach przeciwnie skierowanych niż w grafie pierwotnym
+        // Parametry:
+        //   g - graf wejściowy
+        // Wynik:
+        //   odwrotność grafu
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Graf wynikowy musi być w takiej samej reprezentacji jak wejściowy
+        public DiGraph Lab03Reverse(DiGraph g)
+        {
+            return null;
+        }
+
+        // Część 2
+        // Badanie czy graf jest dwudzielny
+        //   0.5 pkt
+        // Graf dwudzielny to graf nieskierowany, którego wierzchołki można podzielić na dwa rozłączne zbiory
+        // takie, że dla każdej krawędzi jej końce należą do róźnych zbiorów
+        // Parametry:
+        //   g - badany graf
+        //   vert - tablica opisująca podział zbioru wierzchołków na podzbiory w następujący sposób
+        //          vert[i] == 1 oznacza, że wierzchołek i należy do pierwszego podzbioru
+        //          vert[i] == 2 oznacza, że wierzchołek i należy do drugiego podzbioru
+        // Wynik:
+        //   true jeśli graf jest dwudzielny, false jeśli graf nie jest dwudzielny (w tym przypadku parametr vert ma mieć wartość null)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Podział wierzchołków może nie być jednoznaczny - znaleźć dowolny
+        //   3) Pamiętać, że każdy z wierzchołków musi być przyporządkowany do któregoś ze zbiorów
+        //   4) Metoda ma mieć taki sam rząd złożoności jak zwykłe przeszukiwanie (za większą będą kary!)
+        public bool Lab03IsBipartite(Graph g, out int[] vert)
+        {
+            vert = null;
+            return false;
+        }
+
+        // Część 3
+        // Wyznaczanie minimalnego drzewa rozpinającego algorytmem Kruskala
+        //   1 pkt
+        // Schemat algorytmu Kruskala
+        //   1) wrzucić wszystkie krawędzie do "wspólnego worka"
+        //   2) wyciągać z "worka" krawędzie w kolejności wzrastających wag
+        //      - jeśli krawędź można dodać do drzewa to dodawać, jeśli nie można to ignorować
+        //      - punkt 2 powtarzać aż do skonstruowania drzewa (lub wyczerpania krawędzi)
+        // Parametry:
+        //   g - graf wejściowy
+        //   mstw - waga skonstruowanego drzewa (lasu)
+        // Wynik:
+        //   skonstruowane minimalne drzewo rozpinające (albo las)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Wykorzystać klasę UnionFind z biblioteki Graph
+        //   3) Jeśli graf g jest niespójny to metoda wyznacza las rozpinający
+        //   4) Graf wynikowy (drzewo) musi być w takiej samej reprezentacji jak wejściowy
+        public Graph<int> Lab03Kruskal(Graph<int> g, out int mstw)
+        {
+            mstw = 0;
+            return null;
+        }
+
+        // Część 4
+        // Badanie czy graf nieskierowany jest acykliczny
+        //   0.5 pkt
+        // Parametry:
+        //   g - badany graf
+        // Wynik:
+        //   true jeśli graf jest acykliczny, false jeśli graf nie jest acykliczny
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Najpierw pomysleć jaki, prosty do sprawdzenia, warunek spełnia acykliczny graf nieskierowany
+        //      Zakodowanie tego sprawdzenia nie powinno zająć więcej niż kilka linii!
+        //      Zadanie jest bardzo łatwe (jeśli wydaje się trudne - poszukać prostszego sposobu, a nie walczyć z trudnym!)
+        public bool Lab03IsUndirectedAcyclic(Graph g)
+        {
+            return false;
+        }
+
+    }
+
+}
