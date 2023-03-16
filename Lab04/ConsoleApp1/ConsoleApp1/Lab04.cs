e5f2bbac8b393f5c38630895a254ad4f461275a2
PrzeChoj <premysl.choj@gmail.com>
2023-03-16 12:45:13 +0100

    pol rozwiazania drugiej czesci

--- Lab04/ConsoleApp1/ConsoleApp1/Lab04.cs
+++ Lab04/ConsoleApp1/ConsoleApp1/Lab04.cs
@@ -66,62 +66,6 @@ namespace ASD
             return outTable;
         }
         
-        public (HashSet<(int, (int, int))> canGetIntoSet3, HashSet<int> canGetIntoSet1) ModifiedLab04Stage1(DiGraph<int> graph, int start)
-        {
-            var canGetIntoSet = new HashSet<(int, int)>(); // Mozemy sie dostac do item1 zaczynajac w item2
-            var canGetIntoSet3 = new HashSet<(int, (int, int))>(); // Mozemy sie dostac do item1 bedac wczesjiej w item2
-            var canGetIntoSet1 = new HashSet<int>();
-            var canGetIntoSetOld = new HashSet<(int, int)>(); // W poprzednim kroku
-            var canGetIntoSetOld3 = new HashSet<(int, (int, int))>(); // W poprzednim kroku
-            var canGetIntoSetNew = new HashSet<(int, int)>(); // W tym kroku
-            var canGetIntoSetNew3 = new HashSet<(int, (int, int))>(); // W tym kroku
-            
-            canGetIntoSet.Add((start, -1));
-            canGetIntoSet3.Add((start, (-1, -2)));
-            canGetIntoSetOld.Add((start, -1));
-            canGetIntoSetOld3.Add((start, (-1, -2)));
-            canGetIntoSet1.Add(start);
-
-            while (canGetIntoSetOld.Count != 0)
-            {
-                // Dodaj znalezione ostatnio do calosci
-                foreach (var newElemToCanGetIntoSet in canGetIntoSetOld)
-                {
-                    canGetIntoSet.Add(newElemToCanGetIntoSet);
-                }
-                foreach (var newElemToCanGetIntoSet3 in canGetIntoSetOld3)
-                {
-                    canGetIntoSet3.Add(newElemToCanGetIntoSet3);
-                }
-
-                // Dodaj nowe
-                foreach (var canGetIntoSetOldElement in canGetIntoSetOld)
-                {
-                    foreach (int outNeighbor in graph.OutNeighbors(canGetIntoSetOldElement.Item1))
-                    {
-                        bool canGoFurther = graph.GetEdgeWeight(canGetIntoSetOldElement.Item1, outNeighbor) ==
-                                            canGetIntoSetOldElement.Item2;
-                        bool wasThereAlready = canGetIntoSet.Contains((outNeighbor, canGetIntoSetOldElement.Item1));
-                        if (canGoFurther && !wasThereAlready)
-                        {
-                            canGetIntoSetNew.Add((outNeighbor, canGetIntoSetOldElement.Item1));
-                            canGetIntoSetNew3.Add((outNeighbor, (canGetIntoSetOldElement.Item1, canGetIntoSetOldElement.Item2)));
-                            canGetIntoSet.Add((outNeighbor, canGetIntoSetOldElement.Item1));
-                            canGetIntoSet3.Add((outNeighbor, (canGetIntoSetOldElement.Item1, canGetIntoSetOldElement.Item2)));
-                            canGetIntoSet1.Add(outNeighbor);
-                        }
-                    }
-                }
-                
-                canGetIntoSetOld = canGetIntoSetNew;
-                canGetIntoSetOld3 = canGetIntoSetNew3;
-                canGetIntoSetNew = new HashSet<(int, int)>();
-                canGetIntoSetNew3 = new HashSet<(int, (int, int))>();
-            }
-            
-            return (canGetIntoSet3, canGetIntoSet1);
-        }
-
         /// <summary>
         /// Etap 2 - szukanie możliwości przejścia z jednej z grup z `starts` do jednej z grup z `goals`
         /// </summary>
@@ -133,21 +77,80 @@ namespace ASD
         /// jeżeli possible == false to route ustawiamy na null</returns>
         public (bool possible, int[] route) Lab04Stage2(DiGraph<int> graph, int[] starts, int[] goals)
         {
+            var goalsSet = new HashSet<int>(goals);
+            var graphNewStart = new DiGraph<int>(graph.VertexCount + 1);
+            /// graphNewStart będzie tym samym, co graph z dodatkowym wierzchołkiem startowym
+            /// o indeksie graph.VertexCount i krawedziemi do starts o wadze -1
+            /// Wczesniejsze krawedzie o wadze -1 zamienione na wage graph.VertexCount
+            foreach (var edge in graph.DFS().SearchAll())
+            {
+                int newWeight = graph.GetEdgeWeight(edge.From, edge.To);
+                if (newWeight == -1)
+                {
+                    newWeight = graph.VertexCount;
+                }
+                graphNewStart.AddEdge(edge.From, edge.To, newWeight);
+            }
             foreach (int start in starts)
             {
-                (HashSet<(int, (int, int))> canGetIntoSet3, HashSet<int> canGetIntoSet1) = ModifiedLab04Stage1(graph, start);
-                foreach (int goal in goals)
-                {
-                    if (canGetIntoSet1.Contains(goal))
-                    {
-                        
-                        
-                        
-                        int[] myroute = new int[];
+                graphNewStart.AddEdge(graph.VertexCount, start, -1);
+            }
+            // graphNewStart jest juz zbudowany poprawnie
 
-                        return (true, myroute);
+
+            int START = graph.VertexCount;
+            var canGetIntoSet = new HashSet<(int, int)>(); // Mozemy sie dostac do item1 zaczynajac w item2
+            var canGetIntoSetOld = new HashSet<(int, int)>(); // W poprzednim kroku
+            var canGetIntoSetOld3 = new HashSet<(int, (int, int))>(); // W poprzednim kroku
+            var canGetIntoSetNew = new HashSet<(int, int)>(); // W tym kroku
+            var canGetIntoSetNew3 = new HashSet<(int, (int, int))>(); // W tym kroku
+            
+            canGetIntoSet.Add((START, -1));
+            canGetIntoSetOld.Add((START, -1));
+            canGetIntoSetOld3.Add((START, (-1, -2)));
+            
+            if (goalsSet.Contains(START))
+            {
+                // Juz jestes tam gdzie chciales Ignacy xd
+                return (true, new []{START});
+            }
+
+            while (canGetIntoSetOld.Count != 0)
+            {
+                // Dodaj znalezione ostatnio do calosci
+                foreach (var newElemToCanGetIntoSet in canGetIntoSetOld)
+                {
+                    canGetIntoSet.Add(newElemToCanGetIntoSet);
+                }
+
+                // Dodaj nowe
+                foreach (var canGetIntoSetOldElement in canGetIntoSetOld)
+                {
+                    foreach (int outNeighbor in graphNewStart.OutNeighbors(canGetIntoSetOldElement.Item1))
+                    {
+                        bool canGoFurther = graphNewStart.GetEdgeWeight(canGetIntoSetOldElement.Item1, outNeighbor) ==
+                                            canGetIntoSetOldElement.Item2;
+                        bool wasThereAlready = canGetIntoSet.Contains((outNeighbor, canGetIntoSetOldElement.Item1));
+                        if (canGoFurther && !wasThereAlready)
+                        {
+                            if (goalsSet.Contains(outNeighbor))
+                            {
+                                // Znalazlem trase. Ignacy bedzie zadowolony
+                                
+                                return (true, null);
+                            }
+                            
+                            canGetIntoSetNew.Add((outNeighbor, canGetIntoSetOldElement.Item1));
+                            canGetIntoSetNew3.Add((outNeighbor, (canGetIntoSetOldElement.Item1, canGetIntoSetOldElement.Item2)));
+                            canGetIntoSet.Add((outNeighbor, canGetIntoSetOldElement.Item1));
+                        }
                     }
                 }
+                
+                canGetIntoSetOld = canGetIntoSetNew;
+                canGetIntoSetOld3 = canGetIntoSetNew3;
+                canGetIntoSetNew = new HashSet<(int, int)>();
+                canGetIntoSetNew3 = new HashSet<(int, (int, int))>();
             }
             
             return (false, null);
