27a7ba0fccfccd15398b1ccb140d70969d42216a
PrzeChoj <premysl.choj@gmail.com>
2023-04-02 00:00:14 +0200

    Lab 6 czesc 1 dziala ledwo

--- Lab06/ConsoleApp1/ConsoleApp1/Lab06.cs
+++ Lab06/ConsoleApp1/ConsoleApp1/Lab06.cs
@@ -20,14 +20,14 @@ namespace Lab06
         public bool Lab06Stage1(Graph<int> g, (int color, int city)[] keymasterTents, (int color, int cityA, int cityB)[] borderGates, int p)
         {
             int n = g.VertexCount - 1; // wierzchołek 0 nie występuje w zadaniu
-            int p2 = (int)Math.Pow(2, p+1); // p+1, bo kolory tez sa od 1 XD
+            int p2 = (int)Math.Pow(2, p); // UWAGA, bo kolory tez sa od 1 XD
 
             var myMap = new DiGraph<int>(n * p2 + 1); // p2 kopii mapy. Kazda kopia mowi jakiego rodzaje klucze mamy
 
             foreach (var edge in g.BFS().SearchAll())
             {
                 for (int i = 0; i < p2; i++)
-                {
+                { // TODO Dodawac tylko te, co nie maja blokad
                     myMap.AddEdge(i * n + edge.From, i * n + edge.To, edge.Weight);
                     myMap.AddEdge(i * n + edge.To, i * n + edge.From, edge.Weight);
                 }
@@ -37,9 +37,9 @@ namespace Lab06
             // Zrobic przjscia gdy zbieramy klucz
             int[] keysList = new int[n+1]; // Mowi jakie klucze dostaniemy w i-tym
 
-            foreach ((int color, int city) tent in keymasterTents)
-            {
-                keysList[tent.city] |= (1 << tent.color);
+            foreach (var tent in keymasterTents)
+            { // OPTIMIZE Móglbym uzyc przestawien bitowych, bo (1 << (tent.color-1)) == (int)Math.Pow(2, tent.color - 1), ale tak jest czytelniej
+                keysList[tent.city] |= (int)Math.Pow(2, tent.color - 1); // -1, bo klucze bede kodowal od 0 pierwszy bit mowi o pierwszym kluczu
             }
 
             for (int i = 1; i < n+1; i++)
@@ -47,24 +47,23 @@ namespace Lab06
                 if (keysList[i] == 0)
                     continue;
                 
-                for (int j = 0; j < p2; j++)
+                for (int j = 0; j < p2; j++) // OPTIMIZE tu bym mogl tylko pomowe sprawdzic tych, tak, zeby nie bylo tego ifa 2 linijki nizej
                 {
                     if (j == (j | keysList[i]))
                         continue;
-                    myMap.AddEdge(j * n + i, (j | keysList[i]) * n + i, 0);
+                    myMap.AddEdge(j * n + i, (j | keysList[i]) * n + i, 0); // OPTIMIZE zapisac, ze z tego wierzcholka jest krawedz o wadze 0 i tylko nia pozniej isc w Djikstr-ze
                 }
             }
             
-            // zakazac przechodzenia na bramach bede robil w BFS-ie
+            // zakazac przechodzenia na bramach bede robil w Djikstr-ie
             var q = new Queue();
             q.Enqueue(1);
-            var odwiedzone = new HashSet<int>();
-            odwiedzone.Add(1);
+            var odwiedzone = new HashSet<int> { 1 };
 
             while (q.Count != 0)
             {
                 var v = (int)q.Dequeue()!;
-                if (v == n)
+                if (v % n == 0)
                 {
                     return true;
                 }
@@ -74,35 +73,34 @@ namespace Lab06
                     bool juzTuBylem = odwiedzone.Contains(outNeighbor);
                     if (juzTuBylem)
                         continue;
-                    bool jestBramaZamknieta = JestBramaZamknieta(n, v, outNeighbor, borderGates);
-                    if(jestBramaZamknieta)
+                    if(JestBramaZamknieta(n, v, outNeighbor, borderGates))
                         continue;
-                    // TODO Tu bym mogl sprawdzac, czy doszedlem do konca
+                    // OPTIMIZE Tu bym mogl sprawdzac, czy doszedlem do konca
                     odwiedzone.Add(outNeighbor);
                     q.Enqueue(outNeighbor);
-                    // TODO: Zapisywac skad przyszedlem dla ustalenia scieszki?
+                    // TODO: Zapisywac skad przyszedlem dla ustalenia scieszki
                 }
             }
 
             return false;
         }
 
-        private bool JestBramaZamknieta(int n, int from, int to, (int color, int cityA, int cityB)[] borderGates)
+        private static bool JestBramaZamknieta(int n, int from, int to, (int color, int cityA, int cityB)[] borderGates)
         {
             foreach ((int color, int cityA, int cityB) borderGate in borderGates)
             {
                 if (borderGate.cityA != from || borderGate.cityB != to)
                     continue;
-                if (!CanGo(from % n, borderGate.color))
-                    return false;
+                if (!CanGo((from - 1) / n, borderGate.color))
+                    return true;
             }
 
-            return true;
+            return false;
         }
 
-        public bool CanGo(int x, int i)
+        private static bool CanGo(int ownedKeys, int forbiddenColor)
         {
-            return (x & (1 << i)) > 0;
+            return (ownedKeys & forbiddenColor) > 0; // Jesli moge przejsc, to (ownedKeys & forbiddenColor) == forbiddenColor
         }
         
         /// <summary>
