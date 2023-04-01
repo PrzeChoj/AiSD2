488caefafbdd2e802252fddaf947a25443a20029
PrzeChoj <premysl.choj@gmail.com>
2023-04-02 01:00:45 +0200

    Bugfix czesci 1; pierwsza wersja czesci 2

--- Lab06/ConsoleApp1/ConsoleApp1/Lab06.cs
+++ Lab06/ConsoleApp1/ConsoleApp1/Lab06.cs
@@ -78,7 +78,6 @@ namespace Lab06
                     // OPTIMIZE Tu bym mogl sprawdzac, czy doszedlem do konca
                     odwiedzone.Add(outNeighbor);
                     q.Enqueue(outNeighbor);
-                    // TODO: Zapisywac skad przyszedlem dla ustalenia scieszki
                 }
             }
 
@@ -89,7 +88,7 @@ namespace Lab06
         {
             foreach ((int color, int cityA, int cityB) borderGate in borderGates)
             {
-                if (borderGate.cityA != from || borderGate.cityB != to)
+                if (((borderGate.cityA - from) % n != 0) || ((borderGate.cityB - to) % n != 0))
                     continue;
                 if (!CanGo((from - 1) / n, borderGate.color))
                     return true;
@@ -100,7 +99,7 @@ namespace Lab06
 
         private static bool CanGo(int ownedKeys, int forbiddenColor)
         {
-            return (ownedKeys & forbiddenColor) > 0; // Jesli moge przejsc, to (ownedKeys & forbiddenColor) == forbiddenColor
+            return (ownedKeys & (int)Math.Pow(2, forbiddenColor - 1)) > 0; // Jesli moge przejsc, to (ownedKeys & 2^(forbiddenColor-1)) == 2^(forbiddenColor-1)
         }
         
         /// <summary>
@@ -114,8 +113,85 @@ namespace Lab06
         public (bool solutionExists, int solutionLength) Lab06Stage2(Graph<int> g, (int color, int city)[] keymasterTents, (int color, int cityA, int cityB)[] borderGates, int p)
         {
             int n = g.VertexCount - 1; // wierzchołek 0 nie występuje w zadaniu
+            int p2 = (int)Math.Pow(2, p); // UWAGA, bo kolory tez sa od 1 XD
+
+            var myMap = new DiGraph<int>(n * p2 + 1); // p2 kopii mapy. Kazda kopia mowi jakiego rodzaje klucze mamy
+
+            foreach (var edge in g.BFS().SearchAll())
+            {
+                for (int i = 0; i < p2; i++)
+                { // TODO Dodawac tylko te, co nie maja blokad
+                    myMap.AddEdge(i * n + edge.From, i * n + edge.To, edge.Weight);
+                    myMap.AddEdge(i * n + edge.To, i * n + edge.From, edge.Weight);
+                }
+            }
+            // Kopje mapy gotowe
+            
+            // Zrobic przjscia gdy zbieramy klucz
+            int[] keysList = new int[n+1]; // Mowi jakie klucze dostaniemy w i-tym
+
+            foreach (var tent in keymasterTents)
+            { // OPTIMIZE Móglbym uzyc przestawien bitowych, bo (1 << (tent.color-1)) == (int)Math.Pow(2, tent.color - 1), ale tak jest czytelniej
+                keysList[tent.city] |= (int)Math.Pow(2, tent.color - 1); // -1, bo klucze bede kodowal od 0 pierwszy bit mowi o pierwszym kluczu
+            }
+
+            for (int i = 1; i < n+1; i++)
+            {
+                if (keysList[i] == 0)
+                    continue;
+                
+                for (int j = 0; j < p2; j++) // OPTIMIZE tu bym mogl tylko pomowe sprawdzic tych, tak, zeby nie bylo tego ifa 2 linijki nizej
+                {
+                    if (j == (j | keysList[i]))
+                        continue;
+                    myMap.AddEdge(j * n + i, (j | keysList[i]) * n + i, 0); // OPTIMIZE zapisac, ze z tego wierzcholka jest krawedz o wadze 0 i tylko nia pozniej isc w Djikstr-ze
+                }
+            }
+            
+            // zakazac przechodzenia na bramach bede robil w Djikstr-ie
+            var q = new Queue();
+            q.Enqueue(1);
+            var odwiedzone = new HashSet<int> { 1 };
+            var krokWstecz = new Hashtable();
+
+            while (q.Count != 0)
+            {
+                var v = (int)q.Dequeue();
+                if (v % n == 0)
+                {
+                    return (true, GetTrasaLeangth(v, krokWstecz, myMap));
+                }
+
+                foreach (int outNeighbor in myMap.OutNeighbors(v))
+                {
+                    bool juzTuBylem = odwiedzone.Contains(outNeighbor);
+                    if (juzTuBylem)
+                        continue;
+                    if(JestBramaZamknieta(n, v, outNeighbor, borderGates))
+                        continue;
+                    // OPTIMIZE Tu bym mogl sprawdzac, czy doszedlem do konca
+                    odwiedzone.Add(outNeighbor);
+                    q.Enqueue(outNeighbor);
+                    krokWstecz.Add(outNeighbor, v); // Dla ustalenia scieszki
+                }
+            }
 
             return (false, 0);
+        }
+
+        private int GetTrasaLeangth(int outVertex, Hashtable krokWstecz, DiGraph<int> myMap)
+        {
+            int trasaLength = 0;
+            int vertex = outVertex;
+
+            while (vertex != 1)
+            {
+                int vertexPrev = (int)krokWstecz[vertex];
+                trasaLength += myMap.GetEdgeWeight(vertexPrev, vertex);
+                vertex = vertexPrev;
+            }
+            
+            return trasaLength;
         }
     }
 }
