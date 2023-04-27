47e2cea9313d69725829370239f5bf103d65f37b
PrzeChoj <premysl.choj@gmail.com>
2023-04-27 11:44:09 +0200

    Pierwsza wersja - nie dzial xd

--- Lab10/ConsoleApp1/ConsoleApp1/Lab10.cs
+++ Lab10/ConsoleApp1/ConsoleApp1/Lab10.cs
@@ -17,11 +17,102 @@ namespace Lab10
         /// <returns></returns>
         public List<int> FindSeparatingSet(Graph G, List<int> fanclubs, int[] cost, int maxBudget)
         {
+            int foundBestCost = Int32.MaxValue;
+            bool[] foundBestStations = new bool[G.VertexCount];
             
+            int currentCost = 0;
+            bool[] currentStations = new bool[G.VertexCount];
+
+            void FindCost(int v, int currentCost, bool[] currentStations)
+            {
+                if (v == G.VertexCount - 1) // Wszystkie sa ogarniete
+                {
+                    if (thisAreCorrectFanclubs(G, currentStations, fanclubs))
+                    {
+                        // Na pewno currentCost < foundBestCost, bo tu doszlismy 
+                        foundBestCost = currentCost;
+                        foundBestStations = (bool[])currentStations.Clone();
+                    }
+
+                    return;
+                }
+                
+                // Zalozmy, ze do v (razem v) są juz ogarniete
+                int potentialNewCost = currentCost + cost[v + 1];
+                if (potentialNewCost > maxBudget || potentialNewCost >= foundBestCost)
+                {
+                    return;
+                }
+
+                int oldCost = currentCost;
+                currentCost = potentialNewCost;
+                currentStations[v + 1] = true;
+                FindCost(v + 1, currentCost, currentStations);
+                    
+                currentCost = oldCost;
+                currentStations[v + 1] = false;
+                FindCost(v + 1, currentCost, currentStations);
+            }
+
+            FindCost(-1, currentCost, currentStations);
+
+            if (foundBestCost == Int32.MaxValue)
+            {
+                return null;
+            }
+
+            var outStations = new List<int>();
+            for (int i = 0; i < G.VertexCount; i++)
+            {
+                if (foundBestStations[i])
+                {
+                    outStations.Add(i);
+                }
+            }
             
-            return null;
+            return outStations;
         }
 
+        private static bool thisAreCorrectFanclubs(Graph G, bool[] currentStations, List<int> fanclubs)
+        {
+            // moj DFS, ktory szuka scierzek
+            bool DFSOnlyOneFanclub(int funclub)
+            {
+                bool[] visited = (bool[])currentStations.Clone(); // Nie mozemy przejsc przez posterunki, wiec to tak jakbysmy juz w nich byli
+                Stack<int> stack = new Stack<int>();
+
+                visited[funclub] = true;
+                stack.Push(funclub);
+
+                int v;
+
+                while (stack.Count != 0)
+                {
+                    v = stack.Pop();
+                    
+                    if (fanclubs.Contains(v) && v != funclub)
+                        return false;
+
+                    foreach (int i in G.OutNeighbors(v))
+                    {
+                        if (visited[i])
+                            continue;
+                        visited[i] = true;
+                        stack.Push(i);
+                    }
+                }
+
+                return true;
+            }
+
+            foreach (int fanclub in fanclubs)
+            {
+                if (!DFSOnlyOneFanclub(fanclub))
+                    return false;
+            }
+
+            return true;
+        }
 
 
         /// <summary>
