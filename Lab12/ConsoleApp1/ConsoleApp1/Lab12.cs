a173ea31e0354ef6ce30d72e4f4437f0884d516b
PrzeChoj <premysl.choj@gmail.com>
2023-05-18 10:15:53 +0200

    Lab12

---
+++ Lab12/ConsoleApp1/ConsoleApp1/Lab12.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab12
+{
+    public class Transport : MarshalByRefObject
+    {
+        public const double EPS = 10e-9;
+
+        /// <summary>
+        /// Dwuwymiarowy punkt na płaszczyźnie
+        /// </summary>
+        private struct Point
+        {
+            /// <summary>
+            /// Pierwsza współrzędna
+            /// </summary>
+            public double X { get; }
+
+            /// <summary>
+            /// Druga współrzędna
+            /// </summary>
+            public double Y { get; }
+
+            public Point(double x, double y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            /// <summary>
+            /// Zwraca dystans między dwoma punktami
+            /// </summary>
+            /// <param name="p2">Drugi punkt</param>
+            public double DistTo(Point p2)
+            {
+                return Math.Sqrt((X - p2.X) * (X - p2.X) + (Y - p2.Y) * (Y - p2.Y));
+            }
+        }
+
+        /// <summary>
+        /// Odcinek opisany za pomocą dwóch punktów będących jego końcami
+        /// </summary>
+        private class Segment
+        {
+            /// <summary>
+            /// Punkt początkowy
+            /// </summary>
+            public Point Start;
+
+            /// <summary>
+            /// Punkt końcowy
+            /// </summary>
+            public Point End;
+
+            public Segment(Point start, Point end)
+            {
+                this.Start = start;
+                this.End = end;
+            }
+
+            public Segment(double startX, double startY, double endX, double endY)
+            {
+                this.Start = new Point(startX, startY);
+                this.End = new Point(endX, endY);
+            }
+
+            private static double CrossProduct(Point p1, Point p2, Point p3)
+            {
+                return (p2.X - p1.X) * (p3.Y - p1.Y) - (p2.Y - p1.Y) * (p3.X - p1.X);
+            }
+
+            /// <summary>
+            /// Sprawdza czy dwa odcinki się przecinają
+            /// </summary>
+            /// <param name="b">Drugi odcinek</param>
+            public bool Intersects(Segment b)
+            {
+                double d1 = CrossProduct(Start, End, b.Start);
+                double d2 = CrossProduct(Start, End, b.End);
+                double d3 = CrossProduct(b.Start, b.End, Start);
+                double d4 = CrossProduct(b.Start, b.End, End);
+
+                return (d1 * d2 < 0 && d3 * d4 < 0);
+            }
+
+            /// <summary>
+            /// Zwraca punkt przecięcia dwóch odcinków
+            /// </summary>
+            /// <param name="b">Drugi odcinek</param>
+            public Point IntersectionPoint(Segment b)
+            {
+                if (!Intersects(b))
+                    throw new InvalidOperationException("Szukany punkt przecięcia nie istnieje (lub jest końcem któregoś z odcinków)!");
+
+                double determinant = (Start.X - End.X) * (b.Start.Y - b.End.Y) - (Start.Y - End.Y) * (b.Start.X - b.End.X);
+
+                double x = ((Start.X * End.Y - Start.Y * End.X) * (b.Start.X - b.End.X) - (Start.X - End.X) * (b.Start.X * b.End.Y - b.Start.Y * b.End.X)) / determinant;
+                double y = ((Start.X * End.Y - Start.Y * End.X) * (b.Start.Y - b.End.Y) - (Start.Y - End.Y) * (b.Start.X * b.End.Y - b.Start.Y * b.End.X)) / determinant;
+
+                return new Point(x, y);
+            }
+
+            /// <summary>
+            /// Sprawdza czy dwa odcinki są takie same
+            /// </summary>
+            /// <param name="s2">Drugi odcinek</param>
+            /// <returns></returns>
+            public bool Equals(Segment s2)
+            {
+                if (s2 == null)
+                    return false;
+
+                if (Start.X != s2.Start.X || Start.Y != s2.Start.Y || End.X != s2.End.X || End.Y != s2.End.Y)
+                    return false;
+
+                return true;
+            }
+
+            public override string ToString()
+            {
+                return $"({Start.X}, {Start.Y}) - ({End.X}, {End.Y})";
+            }
+        }
+
+        /// <summary>
+        /// Wykorzystany do sortowania odcinków według współrzędnej y punktu przecięcia z pionową prostą o zadanej współrzędnej x
+        /// </summary>
+        private class SegmentComparer : IComparer<Segment>
+        {
+            /// <summary>
+            /// Współrzędna x pionowej prostej
+            /// </summary>
+            public double verticalLineXCoordinate;
+
+            /// <summary>
+            /// Porównuje dwa odcinki i zwraca ich kolejność według współrzędnej y punktu przecięcia z pionową prostą o zadanej współrzędnej x
+            /// </summary>
+            /// <param name="a">Pierwszy odcinek</param>
+            /// <param name="b">Drugi odcinek</param>
+            /// <returns>
+            /// <para>-1 gdy pierwszy odcinek przecina prostą niżej niż drugi odcinek</para>
+            /// <para>0 gdy oba odcinki przecinają prostą w tym samym punkcie</para>
+            /// <para>1 gdy pierwszy odcinek przecina prostą wyżej niż drugi odcinek</para>
+            /// </returns>
+            public int Compare(Segment a, Segment b)
+            {
+                if (a == null || b == null)
+                    throw new InvalidOperationException("Jeden z porównywanych odcinków to null!");
+
+                if (a.Start.X == a.End.X || b.Start.X == b.End.X)
+                    throw new InvalidOperationException("Jeden z porównywanych odcinków jest pionowy!");
+
+                Point s1, s2, e1, e2;
+                if (a.Start.X < a.End.X)
+                {
+                    s1 = a.Start;
+                    e1 = a.End;
+                }
+                else
+                {
+                    s1 = a.End;
+                    e1 = a.Start;
+                }
+                if (b.Start.X < b.End.X)
+                {
+                    s2 = b.Start;
+                    e2 = b.End;
+                }
+                else
+                {
+                    s2 = b.End;
+                    e2 = b.Start;
+                }
+
+                double aIntersection = s1.Y + (verticalLineXCoordinate - s1.X) * (e1.Y - s1.Y) / (e1.X - s1.X);
+                double bIntersection = s2.Y + (verticalLineXCoordinate - s2.X) * (e2.Y - s2.Y) / (e2.X - s2.X);
+                return aIntersection.CompareTo(bIntersection);
+            }
+        }
+
+        /// <summary>
+        /// Y-struktura wykorzystana do trzymania odcinków posortowanych
+        /// według współrzędnej y ich punktu przecięcia z pionową prostą o zadanej współrzędnej x.
+        /// Kolejność sortowania to od najmniejszej do największej współrzędnej y przecięcia z pionową prostą.
+        /// W czasie wykonywanych operacji na strukturze współrzędna x pionowej prostej jest ustawiania automatycznie 
+        /// w zależności od operacji. Przyjęto założenie, że pionowa prosta przesuwa się od lewej do prawej strony.
+        /// </summary>
+        private class YStructure
+        {
+            private SortedSet<Segment> segments;
+            private SegmentComparer segmentComparer;
+
+            public YStructure()
+            {
+                segmentComparer = new SegmentComparer();
+                segments = new SortedSet<Segment>(segmentComparer);
+            }
+
+            /// <summary>
+            /// Wstawia odcinek do struktury. Współrzędna x pionowej prostej ustawiana jest jako
+            /// mniejsza współrzędna x jednego z końców odcinka.
+            /// </summary>
+            /// <param name="s">Dodawany odcinek</param>
+            public void Insert(Segment s)
+            {
+                if (s == null)
+                    throw new ArgumentNullException("Argument s nie może być nullem!");
+
+                segmentComparer.verticalLineXCoordinate = Math.Min(s.Start.X, s.End.X);
+
+                if (segments.Contains(s))
+                    throw new InvalidOperationException("Podany odcinek już znajduje się w strukturze!");
+
+                segments.Add(s);
+            }
+
+            /// <summary>
+            /// Usuwa odcinek ze struktury. Współrzędna x pionowej prostej ustawiana jest jako
+            /// większa współrzędna x jednego z końców odcinka.
+            /// </summary>
+            /// <param name="s">Usuwany odcinek</param>
+            public void Delete(Segment s)
+            {
+                if (s == null)
+                    throw new ArgumentNullException("Argument s nie może być nullem!");
+
+                segmentComparer.verticalLineXCoordinate = Math.Max(s.Start.X, s.End.X);
+
+                if (!segments.Contains(s))
+                    throw new InvalidOperationException("Podany odcinek nie znajduje się w strukturze!");
+
+                segments.Remove(s);
+            }
+
+            /// <summary>
+            /// Zwraca najbliższego sąsiada nad sprawdzanym odcinkiem zgodnie z kolejnością sortowania odcinków.
+            /// Gdy sąsiad nie istnieje zwracany jest null.
+            /// </summary>
+            /// <param name="s">Odcinek</param>
+            public Segment Above(Segment s)
+            {
+                if (s == null)
+                    throw new ArgumentNullException("Argument s nie może być nullem!");
+
+                if (!segments.Contains(s))
+                    throw new InvalidOperationException("Podany odcinek nie znajduje się w strukturze!");
+
+                Segment upperBound = segments.Reverse().First();
+                foreach (Segment segmentAbove in segments.GetViewBetween(s, upperBound))
+                {
+                    if (!s.Equals(segmentAbove))
+                        return segmentAbove;
+                }
+                return null;
+            }
+
+            /// <summary>
+            /// Zwraca najbliższego sąsiada pod sprawdzanym odcinkiem zgodnie z kolejnością sortowania odcinków.
+            /// Gdy sąsiad nie istnieje zwracany jest null.
+            /// </summary>
+            /// <param name="s">Odcinek</param>
+            public Segment Below(Segment s)
+            {
+                if (s == null)
+                    throw new ArgumentNullException("Argument s nie może być nullem!");
+
+                if (!segments.Contains(s))
+                    throw new InvalidOperationException("Podany odcinek nie znajduje się w strukturze!");
+
+                Segment lowerBound = segments.First();
+                foreach (Segment segmentBelow in segments.GetViewBetween(lowerBound, s).Reverse())
+                {
+                    if (!s.Equals(segmentBelow))
+                        return segmentBelow;
+                }
+                return null;
+            }
+
+            /// <summary>
+            /// Zamienia kolejność dwóch sąsiadujących odcinków. Kolejność odpowiada kolejności sortowania odcinków na prawo od ich przecięcia.
+            /// </summary>
+            /// <param name="s1">Pierwszy odcinek</param>
+            /// <param name="s2">Drugi odcinek</param>
+            public void Interchange(Segment s1, Segment s2)
+            {
+                if (!segments.Contains(s1) || !segments.Contains(s2))
+                    throw new InvalidOperationException("Przynajmniej jeden z zamienianych segmentów nie należy do struktury!");
+
+                Segment s1a = Above(s1);
+                Segment s2a = Above(s2);
+                if (!s2.Equals(s1a) && !s1.Equals(s2a))
+                    throw new InvalidOperationException("Próba zamiany kolejności niesąsiednich odcinków!");
+
+                segments.Remove(s1);
+                segments.Remove(s2);
+
+                segmentComparer.verticalLineXCoordinate = s1.IntersectionPoint(s2).X + EPS;
+
+                segments.Add(s1);
+                segments.Add(s2);
+            }
+        }
+
+        /// <summary>
+        /// Rodzaj zdarzenia
+        /// </summary>
+        enum EventType
+        {
+
+        }
+
+        /// <summary>
+        /// Opisuje zdarzenie
+        /// </summary>
+        class SweepEvent
+        {
+
+        }
+
+        /// <summary>
+        /// Wykorzystany do sortowania zdarzeń w odpowiedniej kolejności
+        /// </summary>
+        class SweepEventComparer : IComparer<SweepEvent>
+        {
+            /// <summary>
+            /// Zwraca kolejność dwóch zdarzeń
+            /// </summary>
+            /// <param name="e1">Pierwsze zdarzenie</param>
+            /// <param name="e2">Drugie zdarzenie</param>
+            /// <returns>
+            /// <para>-1 gdy pierwsze zdarzenie jest przed drugim zdarzeniem</para>
+            /// <para>0 gdy oba zdarzenia mają tę samą kolejność</para>
+            /// <para>1 gdy pierwsze zdarzenie jest za drugim zdarzeniem</para>
+            /// </returns>
+            public int Compare(SweepEvent e1, SweepEvent e2)
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Etap 1 - znaleźć liczbę wszystkich stacji przesiadkowych
+        /// </summary>
+        /// <param name="lines">tablica z liniami metra, gdzie każda linia to współrzędne stacji początkowej i końcowej</param>
+        /// <returns>int - liczba wszystkich stacji przesiadkowych</returns>
+        public int Lab12Stage1(((int, int), (int, int))[] lines)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Etap 2 - znaleźć maksymalną długość nieprzerwanego fragmentu metra między dwoma dowolnymi stacjami
+        /// </summary>
+        /// <param name="lines">tablica z liniami metra, gdzie każda linia to współrzędne stacji początkowej i końcowej</param>
+        /// <returns>double - maksymalna długość nieprzerwanego fragmentu metra między dwoma dowolnymi stacjami</returns>
+        public double Lab12Stage2(((int, int), (int, int))[] lines)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ Lab12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using ASD;
+
+namespace Lab12
+{
+    public abstract class Lab12TestCase : TestCase
+    {
+        protected readonly ((int, int), (int, int))[] lines;
+
+        protected Lab12TestCase(((int, int), (int, int))[] lines, double timeLimit, string description) : base(timeLimit, null, description)
+        {
+            this.lines = lines;
+        }
+
+        protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+            var (code, msg) = checkSolution();
+            return (code, $"{msg} [{this.Description}]");
+        }
+
+        protected abstract (Result resultCode, string message) checkSolution();
+
+        public (Result resultCode, string message) OkResult(string message) => (TimeLimit < PerformanceTime ? Result.LowEfficiency : Result.Success, $"{message} {PerformanceTime.ToString("#0.00")}s");
+    }
+
+    public class Stage1TestCase : Lab12TestCase
+    {
+        protected readonly int expectedNumCrossings;
+        protected int result;
+
+        public Stage1TestCase(((int, int), (int, int))[] lines, int expectedNumCrossings, double timeLimit, string description) : base(lines, timeLimit, description)
+        {
+            this.expectedNumCrossings = expectedNumCrossings;
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((Transport)prototypeObject).Lab12Stage1(lines);
+        }
+
+        protected override (Result resultCode, string message) checkSolution()
+        {
+            if (this.expectedNumCrossings != this.result)
+            {
+                return (Result.WrongResult, $"Zwrócono ({result}), powinno być ({expectedNumCrossings})");
+            }
+
+            return OkResult("OK");
+        }
+    }
+
+    public class Stage2TestCase : Lab12TestCase
+    {
+        protected readonly double expectedMaximumDistance;
+        protected readonly double eps;
+        protected double result;
+
+        public Stage2TestCase(((int, int), (int, int))[] lines, double expectedMaximumDistance, double eps, double timeLimit, string description) : base(lines, timeLimit, description)
+        {
+            this.expectedMaximumDistance = expectedMaximumDistance;
+            this.eps = eps;
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((Transport)prototypeObject).Lab12Stage2(lines);
+        }
+
+        protected override (Result resultCode, string message) checkSolution()
+        {
+            if (this.result < 0)
+            {
+                return (Result.WrongResult, $"Zwrócono negatywny dystans ({result}), powinno być ({expectedMaximumDistance})");
+            }
+
+            double absoluteDiff = Math.Abs(this.expectedMaximumDistance - this.result);
+            if (absoluteDiff > this.eps)
+            {
+                return (Result.WrongResult, $"Zwrócono niepoprawny wynik ({result}), powinno być ({expectedMaximumDistance}), wartość bezwzględna różnicy ({absoluteDiff})");
+            }
+
+            return OkResult("OK");
+        }
+    }
+
+
+
+    public class Lab12Tests : TestModule
+    {
+        TestSet Stage1 = new TestSet(prototypeObject: new Transport(), description: "Etap 1", settings: true);
+        TestSet Stage2 = new TestSet(prototypeObject: new Transport(), description: "Etap 2", settings: true);
+
+        public override void PrepareTestSets()
+        {
+            TestSets["Stage1"] = Stage1;
+            TestSets["Stage2"] = Stage2;
+
+            prepare();
+        }
+
+        private void addStage1(Stage1TestCase s1TestCase)
+        {
+            Stage1.TestCases.Add(s1TestCase);
+        }
+
+        private void addStage2(Stage2TestCase s2TestCase)
+        {
+            Stage2.TestCases.Add(s2TestCase);
+        }
+
+
+        private void prepare()
+        {
+            const double timeLimit = 1;
+            const double eps = Transport.EPS;
+            ((int, int), (int, int))[] lines;
+            int expectedNumCrossings;
+            double expectedMaximumDistance;
+            string description;
+
+
+            // TEST 1
+            lines = new ((int, int), (int, int))[]
+            {
+                ((148, 167), (41, 22)), // AB
+                ((124, 86), (65, 103)), // CD
+                ((3, 60), (102, 45)), // EF
+                ((40, 2), (25, 101)), // GH
+                ((47, 142), (66, 123)), // IJ
+            };
+            expectedNumCrossings = 3;
+            expectedMaximumDistance = 90.1027191598567;
+            description = "Przykład z zadania";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 2
+            lines = new ((int, int), (int, int))[]
+            {
+                ((41, 22), (148, 167)),
+                ((65, 103), (124, 86)),
+                ((102, 45), (3, 60)),
+                ((25, 101), (40, 2)),
+                ((66, 123), (47, 142)),
+            };
+            expectedNumCrossings = 3;
+            expectedMaximumDistance = 90.1027191598567;
+            description = "Odwrócony przykład z zadania";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 3
+            lines = new ((int, int), (int, int))[]
+            {
+                ((0, 0), (1, 1)),
+            };
+            expectedNumCrossings = 0;
+            expectedMaximumDistance = 1.4142135623731;
+            description = "Jedna linia metra";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 4
+            lines = new ((int, int), (int, int))[]
+            {
+                ((0, 0), (5, 5)),
+                ((20, 15), (45, 30)),
+            };
+            expectedNumCrossings = 0;
+            expectedMaximumDistance = 29.1547594742265;
+            description = "Dwie linie metra bez przecięcia";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 5
+            lines = new ((int, int), (int, int))[]
+            {
+                ((58, 362), (324, 96)),
+                ((42, 42), (180, 408)),
+                ((408, 180), (44, 82)),
+            };
+            expectedNumCrossings = 3;
+            expectedMaximumDistance = 224.806152676015;
+            description = "Trójkąt";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 6
+            lines = new ((int, int), (int, int))[]
+            {
+                ((88, 164), (242, 52)),
+                ((84, 84), (340, 416)),
+                ((162, 48), (418, 380)),
+                ((220, 410), (412, 260)),
+            };
+            expectedNumCrossings = 4;
+            expectedMaximumDistance = 273.55240539977;
+            description = "Gmina Manhattan";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 7
+            lines = new ((int, int), (int, int))[]
+            {
+                ((0, 0), (2, 2)),
+                ((4, 4), (8, 8)),
+                ((10, 10), (12, 12))
+            };
+            expectedNumCrossings = 0;
+            expectedMaximumDistance = 5.656854249492;
+            description = "Współliniowe metro";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 8
+            lines = new ((int, int), (int, int))[]
+            {
+                ((2, 3), (8, 9)),
+                ((1, 8), (9, 16)),
+                ((3, 6), (10, 13)),
+                ((0, 5), (7, 12)),
+            };
+            expectedNumCrossings = 0;
+            expectedMaximumDistance = 11.3137084989848;
+            description = "Równoległe metro";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 9
+            lines = new ((int, int), (int, int))[]
+            {
+                ((23, 61), (42, 42)),
+                ((62, 43), (41, 22)),
+                ((80, 4), (61, 23)),
+                ((21, 21), (40, 2)),
+                ((63, 63), (82, 44)),
+                ((2, 40), (100, 5)),
+                ((122, 46), (101, 25)),
+            };
+            expectedNumCrossings = 2;
+            expectedMaximumDistance = 44.5982062419555;
+            description = "Mała gmina";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 10
+            lines = new ((int, int), (int, int))[]
+            {
+                ((44, 82), (86, 124)),
+                ((46, 122), (122, 46)),
+                ((165, 108), (81, 24)),
+                ((125, 106), (163, 68)),
+                ((110, 205), (129, 186)),
+                ((63, 63), (120, 6)),
+                ((104, 85), (161, 28)),
+                ((132, 246), (180, 9)),
+                ((88, 164), (145, 107)),
+                ((130, 206), (87, 144)),
+                ((152, 247), (111, 225)),
+                ((127, 146), (85, 104)),
+                ((182, 49), (60, 3)),
+                ((49, 182), (70, 203)),
+                ((89, 184), (28, 161)),
+                ((189, 189), (167, 148)),
+            };
+            expectedNumCrossings = 13;
+            expectedMaximumDistance = 136.131149545367;
+            description = "Sen o Skaryszewie";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+
+
+            // TEST 11
+            lines = new ((int, int), (int, int))[]
+            {
+                ((0, 0), (63, 63)),
+                ((20, 1), (83, 64)),
+                ((40, 2), (103, 65)),
+                ((60, 3), (3, 60)),
+                ((80, 4), (23, 61)),
+                ((100, 5), (43, 62)),
+            };
+            expectedNumCrossings = 9;
+            expectedMaximumDistance = 44.5477272147525;
+            description = "Siatka";
+            addStage1(new Stage1TestCase(lines, expectedNumCrossings, timeLimit, description));
+            addStage2(new Stage2TestCase(lines, expectedMaximumDistance, eps, timeLimit, description));
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new Lab12Tests();
+            tests.PrepareTestSets();
+            foreach (var ts in tests.TestSets)
+            {
+                ts.Value.PerformTests(verbose: true, checkTimeLimit: false);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
