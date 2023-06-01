f17c72d102a78101565d084965d46278d7781ee4
PrzeChoj <premysl.choj@gmail.com>
2023-06-01 11:02:28 +0200

    Czesc 1

--- Lab14/ConsoleApp1/ConsoleApp1/Lab14.cs
+++ Lab14/ConsoleApp1/ConsoleApp1/Lab14.cs
@@ -4,74 +4,44 @@ using System.Linq;
 
 namespace Lab14
 {
-    public class KMPAlgorithm
-    {
-        public static int[] ComputePrefixFunction(string pattern)
-        {
-            int[] prefixFunction = new int[pattern.Length];
-            int i = 1, j = 0;
-
-            while (i < pattern.Length)
-            {
-                if (pattern[i] == pattern[j])
-                {
-                    prefixFunction[i] = j + 1;
-                    i++;
-                    j++;
-                }
-                else
-                {
-                    if (j != 0)
-                    {
-                        j = prefixFunction[j - 1];
-                    }
-                    else
-                    {
-                        prefixFunction[i] = 0;
-                        i++;
-                    }
-                }
-            }
-
-            return prefixFunction;
-        }
-
-        public static int KMPSearch(string text, string pattern)
-        {
-            int[] prefixFunction = ComputePrefixFunction(pattern);
-            int i = 0, j = 0;
-
-            while (i < text.Length && j < pattern.Length)
-            {
-                if (text[i] == pattern[j])
-                {
-                    i++;
-                    j++;
-                }
-                else
-                {
-                    if (j != 0)
-                    {
-                        j = prefixFunction[j - 1];
-                    }
-                    else
-                    {
-                        i++;
-                    }
-                }
-            }
-
-            if (j == pattern.Length)
-            {
-                return i - j; // Pattern found, return the starting index
-            }
-
-            return -1; // Pattern not found
-        }
-    }
-
     public class Lab14 : MarshalByRefObject
     {
+        private static string ReverseString(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private static int[] BuildPrefixTable(string text)
+        {
+            int[] prefixTable = new int[text.Length];
+            int j = 0;
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (text[i] == text[j])
+                {
+                    prefixTable[i] = j + 1;
+                    j++;
+                }
+                else
+                {
+                    if (j != 0)
+                    {
+                        j = prefixTable[j - 1];
+                        i--;
+                    }
+                    else
+                    {
+                        prefixTable[i] = 0;
+                    }
+                }
+            }
+
+            return prefixTable;
+        }
+        
         /// <summary>
         /// Etap 1 - znaleźć najkrótszy palindrom, który można uzyskać poprzez rozszerzanie słowa
         /// </summary>
@@ -79,13 +49,18 @@ namespace Lab14
         /// <returns>string - najkrótszy palindrom uzyskany z wejścia</returns>
         public string Lab14Stage1(string text)
         {
-            text = "ABABDABACDABABCABAB";
-            string pattern = "ABABCABAB";
+            string reversedText = ReverseString(text);
+            string modifiedText = text + "#" + reversedText;
 
-            int index = KMPAlgorithm.KMPSearch(text, pattern);
-
+            int[] prefixTable = BuildPrefixTable(modifiedText);
             
-            return "";
+
+            int palindromeLength = prefixTable[prefixTable.Length - 1];
+            string palindromeSuffix = text.Substring(palindromeLength);
+
+            string shortestPalindrome = ReverseString(palindromeSuffix) + text;
+
+            return shortestPalindrome;
         }
 
         /// <summary>
